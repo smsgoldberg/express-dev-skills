2eb379ebd0029a89e84219614cad54a52c4a25c5
Sarah <sarahspagnolagoldberg@gmail.com>
2023-08-15 21:28:52 -0400

    add code to routes/index.js

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index.ejs', { title: 'Express Skills' });
 });
 
+//GET /skills
+router.get('/', skillsCtrl.index);
+//GET /skills/:id
+router.get('/:id', skillsCtrl.show);
+
 module.exports = router;
